6af32f8235ad723312a5b556433a4a9d4cd9b9c5
IlyaHarelik <ilyha.gor@mail.ru>
2022-09-07 16:15:43 +0300

    finish

--- Task2.php
+++ Task2.php
@@ -3,20 +3,18 @@
 namespace src;
 
 use DateTime;
+use InvalidArgumentException;
 
 class Task2
 {
     public function main(string $date): string
     {
-        date_default_timezone_set('UTC+3');
-        $dateBirth = date_create_from_format("j.m.Y", $date);
+        $dateBirth = date_create_from_format("j-m-Y", $date);
         $currentDate = new DateTime('now');
         $diff = date_diff($currentDate, $dateBirth);
-
-        return $diff->format('%R%a days');
+        $daysLeft = $diff->format('%R%a');
+        if($daysLeft < 0 )
+            throw new InvalidArgumentException;
+        return $daysLeft;
     }
 }
-
-$birthdayCountdown = new Task2;
-
-echo $birthdayCountdown->main('21.02.2025');
