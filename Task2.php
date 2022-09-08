28e1d83717e73e20f5fec1b0a2a92612a774ab78
IlyaHarelik <ilyha.gor@mail.ru>
2022-09-08 10:50:04 +0300

    finish

--- Task2.php
+++ Task2.php
@@ -7,12 +7,16 @@ use InvalidArgumentException;
 
 class Task2
 {
-    public function main(string $date): string
+    public function main(string $date): int
     {
+        if (!(preg_match("/^(0[1-9]|[12][0-9]|3[01])[\-](0[1-9]|1[012])[\-](19|20)\d\d$/", $date))) {
+            throw new InvalidArgumentException;
+        }
         $dateBirth = date_create_from_format("j-m-Y", $date);
         $currentDate = new DateTime('now');
         $diff = date_diff($currentDate, $dateBirth);
         $daysLeft = $diff->format('%R%a');
+        $daysLeft = trim($daysLeft, $characters = "+");
         if($daysLeft < 0 )
             throw new InvalidArgumentException;
         return $daysLeft;
