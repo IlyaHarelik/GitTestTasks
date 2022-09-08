e8d4c88f2c0f73aa0a27d1b6f61b25bdf10c6e20
IlyaHarelik <ilyha.gor@mail.ru>
2022-09-08 12:51:47 +0300

    csfixed

--- Task2.php
+++ Task2.php
@@ -12,14 +12,15 @@ class Task2
         if (!(preg_match("/^(0[1-9]|[12][0-9]|3[01])[\-](0[1-9]|1[012])[\-](19|20)\d\d$/", $date))) {
             throw new InvalidArgumentException();
         }
-        $dateBirth = date_create_from_format("j-m-Y", $date);
+        $dateBirth = date_create_from_format('j-m-Y', $date);
         $currentDate = new DateTime('now');
         $diff = date_diff($currentDate, $dateBirth);
         $daysLeft = $diff->format('%R%a');
-        $daysLeft = trim($daysLeft, $characters = "+");
+        $daysLeft = trim($daysLeft, $characters = '+');
         if ($daysLeft < 0) {
             throw new InvalidArgumentException();
         }
+
         return $daysLeft;
     }
 }
